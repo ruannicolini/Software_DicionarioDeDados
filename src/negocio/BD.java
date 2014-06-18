1de64a23fbfd625ea19ff5e20fc1ae25fe0d0b2c
Ruan Nicolini <ruannicolini@gmail.com>
2014-06-18 14:45:32 -0300

    Commit Inicial

---
+++ src/negocio/BD.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+/**
+ *
+ * @author Ruan
+ */
+public class BD {
+    int id;
+    String nome;
+    String descricao;
+
+    public BD(int id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
---
+++ src/negocio/Campo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+/**
+ *
+ * @author Ruan
+ */
+public class Campo {
+    int id;
+    String nome;
+    String tipo;
+    String descricao;
+
+    public Campo(int id, String nome, String tipo, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
